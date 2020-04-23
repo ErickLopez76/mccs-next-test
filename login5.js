c7b577b62f43bc8614504d84455cad29d1df41f5
Erick Lopz <ericklopez76@gmail.com>
2020-04-23 09:58:28 -0600

    Add files via upload
    This js file call API when make click in a button, and get 405 error

---
+++ login5.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Link from 'next/link.js';
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
+
+// set up cookies
+const cookies = new Cookies();
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: cookies.get('token') || null
+    }
+  }
+
+//   onLoginClick = async () => {
+async onLoginClick(event){
+    // event.preventDefault()
+    console.log("Login called");
+    let mail = "user@dev.null";
+    let pass = "1TrulySecurePassword!!";
+    let a = "1";
+    const response = await axios.get("http://localhost:8080/api/v1/login", {
+      method: "POST",
+      timeout: 0,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({ email: mail, password: pass }),
+    });
+  
+    console.log(response.data)
+    const token = response.data.token;
+    cookies.set('token', token);
+    this.setState({
+      token: token
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Main page</h2>
+        <br></br>
+        <button onClick={() => this.onLoginClick()}>Get Token</button>
+        <br></br>
+        <p>Token: {this.state.token}</p>
+        <br></br>
+      </div >
+    )
+  }
+}
+
+export default Index;
